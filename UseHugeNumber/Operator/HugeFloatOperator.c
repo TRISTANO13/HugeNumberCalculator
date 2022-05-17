788f7af8b81f7da85cf2c96e764cce9926efc9ae
Quezzar <98542233+Quezzar@users.noreply.github.com>
2022-05-17 09:55:52 +0200

    Update HugeFloatOperator.c

--- UseHugeNumber/Operator/HugeFloatOperator.c
+++ UseHugeNumber/Operator/HugeFloatOperator.c
@@ -6,7 +6,21 @@ HugeFloat* addHugeFloat (const HugeFloat* operand1, const HugeFloat* operand2) {
 }
 
 HugeFloat* substractHugeFloat (const HugeFloat* operand1, const HugeFloat* operand2) {
-    return NULL;
+    HugeFloat* result;
+    HugeFloat* tmpOperand2 = createHugeFloatFromHugeFloat (operand2, 0);
+
+    if (tmpOperand2 == NULL) {
+        return NULL;
+    }
+    if (tmpOperand2->significand->sign == PLUS) {
+        tmpOperand2->significand->sign = MINUS;
+    } else {
+        tmpOperand2->significand->sign = PLUS;
+    }
+    result = addHugeFloat (operand1, tmpOperand2);
+    deleteHugeFloat (tmpOperand2);
+    simplifyHugeFloat (result);
+    return result;
 }
 
 HugeFloat* multiplyHugeFloat (const HugeFloat* operand1, const HugeFloat* operand2) {
