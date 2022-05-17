698af6ed4bdc93424cf3b49eb0a48037ccdd7ab3
TRISTANO13 <93603853+TRISTANO13@users.noreply.github.com>
2022-05-17 10:00:25 +0200

    Update HugeIntOperator.c

--- UseHugeNumber/Operator/HugeIntOperator.c
+++ UseHugeNumber/Operator/HugeIntOperator.c
@@ -14,5 +14,14 @@ HugeInt* multiplyHugeInt (const HugeInt* operand1, const HugeInt* operand2) {
 }
 
 HugeInt* divideHugeInt (const HugeInt* operand1, const HugeInt* operand2) {
-    return NULL;
+    HugeInt* result = createHugeIntFromString ("0");
+    if (result != NULL) {
+        result->absoluteValue = divideHugeUnsignedInt (operand1->absoluteValue, operand2->absoluteValue);
+        if (operand1->sign == operand2->sign) {
+            result->sign = PLUS;
+        } else {
+            result->sign = MINUS;
+        }
+    }
+    return result;
 }
