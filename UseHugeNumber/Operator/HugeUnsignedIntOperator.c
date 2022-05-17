56e81d499d366fe2739125bcf6dae43bb2a86199
AntoineCmbld <74043876+AntoineCmbld@users.noreply.github.com>
2022-05-17 09:58:59 +0200

    Update HugeUnsignedIntOperator.c

--- UseHugeNumber/Operator/HugeUnsignedIntOperator.c
+++ UseHugeNumber/Operator/HugeUnsignedIntOperator.c
@@ -16,7 +16,41 @@ HugeInt* substractHugeUnsignedInt (const HugeUnsignedInt* operand1, const HugeUn
 }
 
 HugeUnsignedInt* multiplyHugeUnsignedInt (const HugeUnsignedInt* operand1, const HugeUnsignedInt* operand2) {
-    return NULL;
+    if ((operand1 == NULL) || (operand2 == NULL)) {
+        return NULL;
+    }
+    HugeUnsignedInt* result = createHugeUnsignedInt ();
+    if (result == NULL) {
+        return NULL;
+    }
+    unsigned int column = 0;
+    for (Node* node2 = operand2->end; !isNodeEmpty (node2); node2 = node2->previous, column++) {
+        Digit carry = 0;
+        HugeUnsignedInt* temp = createHugeUnsignedInt ();
+        if (temp == NULL) {
+            return NULL;
+        }
+        for (Node* node1 = operand1->end; !isNodeEmpty (node1); node1 = node1->previous) {
+           Digit digitNode1 = node1->digit;
+            Digit digitNode2 = node2->digit;
+            Digit digitResult = ((digitNode1 * digitNode2) + carry) % 10;
+            carry = ((digitNode1 * digitNode2) + carry) / 10;
+            addDigitAtStart (temp, digitResult);
+        }
+        if (carry > 0) {
+            addDigitAtStart (temp, carry);
+        }
+        for (unsigned int i = 0; i < column; i++) {
+            addDigitAtEnd (temp, 0);
+        }
+        HugeUnsignedInt* tmpResult = addHugeUnsignedInt (result, temp);
+        deleteHugeUnsignedInt (result);
+        result = tmpResult;
+        deleteHugeUnsignedInt (temp);
+    }
+
+    simplifyHugeUnsignedInt (result);
+    return result;
 }
 
 HugeUnsignedInt* divideHugeUnsignedInt (const HugeUnsignedInt* operand1, const HugeUnsignedInt* operand2) {
